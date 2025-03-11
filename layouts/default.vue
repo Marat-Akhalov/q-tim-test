934d4d3672951ce0bd980d4caaf932b4976e9d4b
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2025-03-11 08:54:16 +0400

    feat: add default page and layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="page-wrap">
+    <div class="container">
+      <main>
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page-wrap {
+  display: flex;
+  flex-direction: column;
+  min-height: 100dvh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Test page</div>
+</template>
+
+<style scoped lang="scss"></style>
