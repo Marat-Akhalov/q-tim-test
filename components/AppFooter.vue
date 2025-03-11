7488d3cb3b47331e15a92f6e9dc57d00e351fa0b
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2025-03-11 09:18:30 +0400

    feat: add initial markup for footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__left">
+      <span class="footer__text"> Is there a project?</span>
+      <div class="footer__heading">Let&rsquo;s work together</div>
+    </div>
+    <div class="footer__right">
+      <ul class="footer__contacts contacts list-reset">
+        <li class="contacts__item">
+          <h3 class="contacts__header">For all questions</h3>
+          <a
+            href="tel:+79202780068"
+            class="contacts__link contacts__link--tel"
+            >+7 (920) 278—00—68</a
+          >
+          <a
+            href="mailto:qtim@agency.com"
+            class="contacts__link contacts__link--mail"
+            >qtim@agency.com</a
+          >
+        </li>
+        <li class="contacts__item">
+          <h3 class="contacts__header">Office in Russia</h3>
+          <span class="contacts__address">
+            139 Ruben Station Lake Drew, NM 52678
+          </span>
+          <a
+            href="https://google.com"
+            class="contacts__social"
+          >
+            <img
+              src="@/assets/img/social-logo.svg"
+              alt="Логотип соцсети"
+            />
+          </a>
+        </li>
+        <li class="contacts__item">
+          <h3 class="contacts__header">Office in Dubai</h3>
+          <span class="contacts__address">
+            139 Ruben Station Lake Drew, NM 52678
+          </span>
+          <a
+            href="https://google.com"
+            class="contacts__social"
+          >
+            <img
+              src="@/assets/img/social-logo.svg"
+              alt="Логотип соцсети"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss"></style>
